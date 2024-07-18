c9d3f4f243097081f735b3a9bd8376ad6d12282e
Vanessa Maher <vanessa.maher@keyin.com>
2024-07-18 21:21:17 -0230

    Add files via upload

--- WebDevQAP4/qap4.js
+++ WebDevQAP4/qap4.js
@@ -1,51 +1,73 @@
-// let motelCustomer = {
+// Example blank template:
+//
 //     name: "",
 //     dateOfBirth: "",
 //     gender: "",
 //     roomPreference: ["early check-in", "extra bed", "extra key", "late check-out"],
+//     roomNumber: "",
 //     paymentMethod: "",
 //     mailingAddress: {
 //         street: "",
 //         city: "",
 //         province: "",
-//         postalCode: ""
+//         postalCode: "",
 //     },
 //     phoneNumber: "",
-//     checkInDate: {
-//         date: "",
-//         checkOutDate: ""
-//     },
-// }
+//     emailAddress: "",
+//     checkInDate: "",
+//     checkOutDate: "", 
 
 let motelCustomer = {
     name: "Billy Bob Smith",
     dateOfBirth: "1985-06-01",
     gender: "M",
-    roomPreference: ["early check-in", "extra bed", "extra key", "late check-out"],
+    roomPreference: ["extra bed", "extra key", "late check-out"],
+    roomNumber: "344",
     paymentMethod: "Credit",
     mailingAddress: {
         street: "12 Main Street",
         city: "Toronto",
         province: "ON",
-        postalCode: "M4C 2J4"
+        postalCode: "M4C 2J4",
     },
     phoneNumber: "7095419861",
-    checkInDate: {
-        date: "2024-07-14",
-        checkOutDate: "2024-07-16"
-    }
+    emailAddress: "billybobsmith@gmail.com",
+    checkInDate: "2024-07-14",
+    checkOutDate: "2024-07-16", 
+
+    determineStayLength: function() {
+        return (new Date(this.checkOutDate) - new Date(this.checkInDate)) / (24 * 60 * 60 * 1000);
+    },
+    determineAge: function() {
+        const birthDate = new Date(this.dateOfBirth);
+        const today = new Date();
+        let age = today.getFullYear() - birthDate.getFullYear();
+        if (today < new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    },
+    determinePronoun: function() {
+        if (this.gender === "M") {
+            this.pronounInCaps = "He";
+            this.pronounNormal = "he";
+        }
+        else if (this.gender === "F") {
+            this.pronounInCaps = "She";
+            this.pronounNormal = "she";
+        }
+    },
 };
 
-if (motelCustomer.gender === "M") {
-    pronounInCaps = "He"; }
-else if (motelCustomer.gender === "F") {
-    pronounInCaps = "She"; }
+motelCustomer.determinePronoun();
 
 let motelCustomerDescription = `The customer's name is ${motelCustomer.name}.
-${pronounInCaps} was born on ${motelCustomer.dateOfBirth}.
-The mailing address on file is ${motelCustomer.mailingAddress.street}, ${motelCustomer.mailingAddress.city}, ${motelCustomer.mailingAddress.province}, ${motelCustomer.mailingAddress.postalCode} 
-The phone number on file is ${motelCustomer.phoneNumber}. 
-Room preferences indicated were ${motelCustomer.roomPreference.join(', ')}. 
-Check-in date was ${motelCustomer.checkInDate.date} and check-out date was ${motelCustomer.checkInDate.checkOutDate}.`;
+${motelCustomer.pronounInCaps} was born on ${motelCustomer.dateOfBirth}, meaning ${motelCustomer.pronounNormal} is ${motelCustomer.determineAge()} years old.
+The mailing address on file is ${motelCustomer.mailingAddress.street}, ${motelCustomer.mailingAddress.city}, ${motelCustomer.mailingAddress.province}, ${motelCustomer.mailingAddress.postalCode}. 
+The phone number on file is ${motelCustomer.phoneNumber}, and the email address on file is ${motelCustomer.emailAddress}. 
+Room preferences indicated were ${motelCustomer.roomPreference.join(', ')}.
+${motelCustomer.name} was located in room number ${motelCustomer.roomNumber}.
+Check-in date was ${motelCustomer.checkInDate} and check-out date was ${motelCustomer.checkOutDate}.
+The duration of the stay was ${motelCustomer.determineStayLength()} days.`;
 
 console.log(motelCustomerDescription);
